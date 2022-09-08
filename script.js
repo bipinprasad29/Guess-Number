c7083771cb3c6b274b09238defd5b5463b671759
bipinprasad29 <103585033+bipinprasad29@users.noreply.github.com>
2022-09-08 17:42:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,35 @@
-let box = document.getElementsByClassName("box");
-let winArr=[];
-let turn = "X";
-const changeTurn = () => turn === "X" ? "O" : "X"
-let noOfturn =0
-let w = document.getElementById("winner");
-let winmsg = document.getElementById("winner");
+    const id = (id) => document.getElementById(id);
+      let num = id("number"),
+        andomnum,
+        msg = id("msg"),
+        btn = id("btn");
 
-// winning condition for plyers 
-const CheckWin = () => {
-  let boxtexts = document.getElementsByClassName("boxtext");
-  let win = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < win.length; i++) {
-    const [a, b, c] = win[i];
-    if (
-      boxtexts[a].innerHTML &&
-      boxtexts[a].innerHTML === boxtexts[b].innerHTML &&
-      boxtexts[a].innerHTML === boxtexts[c].innerHTML
-    ) {
-      winArr.push('true');
-     
-      w.style.display = "block";
-      boxtexts[a].style.color =
-        boxtexts[c].style.color =
-        boxtexts[b].style.color =
-          "rgb(255, 255, 255)";
-      w.style.color ="#fff";
-      w.innerHTML = boxtexts[a].innerHTML + " " + w.innerHTML;
-    }
-  }
-};
+      let count = document.querySelector(".count");
+      let a = 0;
+      count.innerHTML = "No. of Attempt: " + a;
+      btn.addEventListener("click", () => {
+        if (num.value) {
+          a++;
+          count.innerHTML = "No. of Attempt: " + a;
+          randomnum = Math.floor(Math.random() * 100 + 1);
+          if (num.value == randomnum) {
+            msg.style.display = "block";
+            msg.innerText = `You Guessed the right no. in ${a} attempt :)`;
+            msg.style.color = "green";
+            restart();
+          } else {
+            msg.style.display = "block";
+            msg.innerText = `You Guessed the wrong no. :(
+                              the no. is ${randomnum}`;
+            msg.style.color = "red";
+          }
+        } else {
+          msg.innerText = "Please Enter a valid number";
+          msg.style.color = "red";
+        }
+      });
 
-// checking for draw
-const checDraw =()=>{
-  if(noOfturn >= 9 && winArr.length <1){
-    w.style.display = "block";
-    w.style.color ="#2b3259";
-    w.innerHTML = 'its a Draw';
-  }
-}
-
-Array.from(box).forEach((element) => {
-  let boxtext = element.querySelector(".boxtext");
-  element.addEventListener("click", () => {
-    if(winArr.length<1)
-    {
-      if (boxtext.innerText === "") {
-        noOfturn++
-      boxtext.innerText = turn;
-      turn = changeTurn();
-      document.getElementById("player").innerText = turn + "-turn";
-      CheckWin();
-      checDraw()
-    }}
-  });
-});
-
-reset.addEventListener("click", () => {
-  let boxtexts = document.querySelectorAll(".boxtext");
-  Array.from(boxtexts).forEach((element) => {
-    element.innerText = "";
-    element.style.color = "black";
-    winArr=[]
-    turn = "X";
-    document.getElementById("player").innerText = "X-turn";
-    let wi = document.getElementById("winner");
-    wi.style.display = "none";
-    wi.innerText = "is the winner";
-    noOfturn= 0;
-  });
-});
+      let restart = () => {
+        num.value = "";
+        a = 0;
+      };
