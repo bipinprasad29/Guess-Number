38a7302b01950d8aa1aa25f643c2a9ae8ae9cfc4
Bipin Prasad <103585033+bipinprasad29@users.noreply.github.com>
2023-02-20 15:43:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
     const id = (id) => document.getElementById(id);
       let num = id("number"),
-        andomnum,
+        randomnum,
         msg = id("msg"),
         btn = id("btn");
 
       let count = document.querySelector(".count");
-      let a = 0;
-      count.innerHTML = "No. of Attempt: " + a;
+      let  noOfAttempt = 0;
+      count.innerHTML = "No. of Attempt: " +  noOfAttempt;
       btn.addEventListener("click", () => {
         if (num.value) {
-          a++;
-          count.innerHTML = "No. of Attempt: " + a;
+           noOfAttempt++;
+          count.innerHTML = "No. of Attempt: " +  noOfAttempt;
           randomnum = Math.floor(Math.random() * 100 + 1);
           if (num.value == randomnum) {
             msg.style.display = "block";
-            msg.innerText = `You Guessed the right no. in ${a} attempt :)`;
+            msg.innerText = `You Guessed the right no. in ${ noOfAttempt} attempt :)`;
             msg.style.color = "green";
             restart();
           } else {
@@ -24,12 +24,13 @@
             msg.style.color = "red";
           }
         } else {
+          msg.style.display = "block";
           msg.innerText = "Please Enter a valid number";
-          msg.style.color = "red";
+          msg.style.color = "blue";
         }
       });
 
       let restart = () => {
         num.value = "";
-        a = 0;
+         noOfAttempt = 0;
       };
